77f982013e8956cce627ad92191a99676dd03174
Leonardo Lobato <leonardo@skyworkz.nl>
2023-11-09 09:33:17 +0100

    adding ebs-csi policy and ecr deployment action

--- terraform/aws/iam-ebs-csi-add-on-policy.tf
+++ terraform/aws/iam-ebs-csi-add-on-policy.tf
@@ -3,30 +3,30 @@
 # # We leverage IRSA in order to link Kubernetes deployments to AWS IAM roles
 # # More info: https://aws.amazon.com/blogs/opensource/introducing-fine-grained-iam-roles-service-accounts/
 # #
-# data "aws_iam_policy_document" "external_dns_assume_role_policy" {
-#   statement {
-#     actions = ["sts:AssumeRoleWithWebIdentity"]
-#     effect  = "Allow"
+data "aws_iam_policy_document" "ebs_csi_driver_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
 
-#     condition {
-#       test     = "StringEquals"
-#       variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
-#       values   = ["system:serviceaccount:kube-system:external-dns"]
-#     }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller"]
+    }
 
-#     principals {
-#       identifiers = [module.eks.oidc_provider_arn]
-#       type        = "Federated"
-#     }
-#   }
-# }
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
 
-# resource "aws_iam_role" "external_dns_role" {
-#   name               = "external-dns-role"
-#   assume_role_policy = data.aws_iam_policy_document.external_dns_assume_role_policy.json
+resource "aws_iam_role" "ebs-csi-driver-role" {
+  name               = "ebs-csi-driver-role"
+  assume_role_policy = data.aws_iam_policy_document.ebs_csi_driver_assume_role_policy.json
 
-#   inline_policy {
-#     name   = "external-dns-policy"
-#     policy = file("${path.module}/iam-policies/external-dns-policy.json")
-#   }
-# }
+  inline_policy {
+    name   = "ebs-csi-driver-policy"
+    policy = file("${path.module}/iam-policies/ebs-csi-driver-policy.json")
+  }
+}
