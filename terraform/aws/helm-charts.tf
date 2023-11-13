051c78a2ea634b55307fcd7cf267f1529cc6df13
Leonardo Lobato <leonardo@skyworkz.nl>
2023-11-13 13:57:41 +0100

    code to deploy argocd helm and crd by terraform

---
+++ terraform/aws/helm-charts.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_manifest" "argo_cd_crds" {
+  manifest = yamldecode(file("../../kubernetes/init/customresourcedefinition-applications.argoproj.io.yaml"))
+}
+
+resource "helm_release" "argo_cd" {
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "5.51.0"
+
+  values = [
+    "${file("../../kubernetes/argocd/values.yaml")}"
+  ]
+
+  depends_on = [kubernetes_manifest.argo_cd_crds]
+}
