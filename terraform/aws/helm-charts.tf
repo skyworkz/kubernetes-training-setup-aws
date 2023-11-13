d71c75ea613618d91e3b4fa35e43a16f94b878b4
Leonardo Lobato <leonardo@skyworkz.nl>
2023-11-13 16:35:45 +0100

    commeting helm chart install for argocd and crds

--- terraform/aws/helm-charts.tf
+++ terraform/aws/helm-charts.tf
@@ -1,16 +1,17 @@
-resource "kubernetes_manifest" "argo_cd_crds" {
-  manifest = yamldecode(file("../../kubernetes/init/customresourcedefinition-applications.argoproj.io.yaml"))
-}
+// TODO uncomment once correct configuration are in place
+# resource "kubernetes_manifest" "argo_cd_crds" {
+#   manifest = yamldecode(file("../../kubernetes/init/customresourcedefinition-applications.argoproj.io.yaml"))
+# }
 
-resource "helm_release" "argo_cd" {
-  name       = "argo-cd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "5.51.0"
+# resource "helm_release" "argo_cd" {
+#   name       = "argo-cd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   chart      = "argo-cd"
+#   version    = "5.51.0"
 
-  values = [
-    "${file("../../kubernetes/argocd/values.yaml")}"
-  ]
+#   values = [
+#     "${file("../../kubernetes/argocd/values.yaml")}"
+#   ]
 
-  depends_on = [kubernetes_manifest.argo_cd_crds]
-}
+#   depends_on = [kubernetes_manifest.argo_cd_crds]
+# }
