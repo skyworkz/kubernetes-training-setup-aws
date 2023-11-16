dcf0a11c11126af0e19168c4f62bbbad38a99821
Leonardo Lobato <leonardo@skyworkz.nl>
2023-11-16 18:09:52 +0100

    adding clusterName as input

--- terraform/aws/_versions.tf
+++ terraform/aws/_versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 0.13.1"
 
+  backend "s3" {
+    bucket = "skyworkz-tf-state-k8s-training"
+    key = "terraform.tfstate"
+    region = "eu-west-1"
+  }
+
+
   required_providers {
     aws        = ">= 5.24.0"
     local      = ">= 1.4"
