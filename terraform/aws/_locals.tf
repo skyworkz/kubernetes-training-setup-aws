679fc4782bd1409afa75ef92f4eae37fc20535c6
Leonardo Lobato <leonardo@skyworkz.nl>
2023-11-16 17:53:29 +0100

    chaning cluster name

--- terraform/aws/_locals.tf
+++ terraform/aws/_locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name            = "skyworkz-k8s-training-${random_string.suffix.result}"
+  name            = "skyworkz-k8s-training"
   cluster_version = "1.28"
   region          = "eu-west-1"
 }
