d00a5ed1efb90f9ad5cc2e88e0f6810cbfe61635
Robert J Czanik <robert@skyworkz.nl>
2023-11-24 17:04:39 +0100

    refactor: changed ip ranges of subnets due to free subscription

--- terraform/aks/terraform/4-subnets.tf
+++ terraform/aks/terraform/4-subnets.tf
@@ -1,13 +1,13 @@
 resource "azurerm_subnet" "subnet-public" {
   name                 = "subnet-public"
-  address_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]#, "10.0.2.0/24", "10.0.3.0/24"]
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
 }
 
 resource "azurerm_subnet" "subnet-private" {
   name                 = "subnet-private"
-  address_prefixes     = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  address_prefixes     = ["10.0.11.0/24"]#, "10.0.12.0/24", "10.0.13.0/24"]
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
 }
